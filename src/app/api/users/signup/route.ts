2cabab9ab4f4f725da6d6820eb462ad09659d9d2
LikhithPalya <likhithpalya@gmail.com>
2024-12-16 20:21:59 +0530

    checked for signup route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -9,41 +9,46 @@ connect()
 
 export async function POST(request: NextRequest){
     try {
-        const requestBody = await request.json() //ways of getting a request
-        const {username, email, password} = requestBody
-        //Validation 
-        console.log(requestBody);
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
 
+        console.log(reqBody);
+
+        //check if user already exists
         const user = await User.findOne({email})
+
         if(user){
-            return NextResponse.json({error:"user already exists"}, {status: 400})
+            return NextResponse.json({error: "User already exists"}, {status: 400})
         }
-        // hashing password using bcrypt
-        
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(password,salt)
-        
+
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
         const newUser = new User({
             username,
             email,
-            password:hashedPassword
+            password: hashedPassword
         })
+
         const savedUser = await newUser.save()
         console.log(savedUser);
 
-        //send the verification email!
-        await sendEmail({email,emailType:"VERIFY", userId: savedUser._id})
-        
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
+
         return NextResponse.json({
-            username,
-            email,
-            savedUser,
-            message:"user registered successfully",
-            success:true
+            message: "User created successfully",
+            success: true,
+            savedUser
         })
         
-    } catch (error:any) {
-        return NextResponse.json({error:error.message}, {status: 500})
-        console.log(error);
+        
+
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+
     }
 }
