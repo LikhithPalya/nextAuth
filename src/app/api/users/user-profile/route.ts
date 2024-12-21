a6881f4b3b5ec431f694ab44ff0e346cbd556401
LikhithPalya <likhithpalya@gmail.com>
2024-12-21 08:55:03 +0530

    completed backend

---
+++ src/app/api/users/user-profile/route.ts
@@ -0,0 +1,28 @@
+import { connect } from "@/db/db.connect";
+import {NextRequest, NextResponse} from 'next/server'
+import { getDataFromToken } from "@/helper/GetDataFromToken";
+import User from "@/models/user.model";
+
+connect()
+
+export async function(request: NextRequest){
+    //extract data from request
+    const userId = await getDataFromToken(request)
+    const user = await User.findById(userId).select("-password")
+    // .then((user)=>{
+    //     return NextResponse.json({
+    //         message: user,
+    //         success: true
+    //     })
+    // })
+
+    //check if there is no user
+    if(!user){
+        return NextResponse.json({error: "User not found"}, {status: 400})
+    }
+
+    return NextResponse.json({
+        message: user,
+        success: true
+    })
+}
